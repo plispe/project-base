34638874e2c10a0da13f50b708307b1f6d525949
Petr Pliska <petr.pliska@post.cz>
2018-11-14 07:59:23 +0100

    Change default region

--- infrastructure/google-cloud/variables.tf
+++ infrastructure/google-cloud/variables.tf
@@ -1,5 +1,10 @@
 variable "GOOGLE_CLOUD_REGION" {
-  type = "string"
+  type        = "string"
   description = "Google cloud region for project resources"
-  default = "europe-west4" # we have to switch to europe-west4 because of redis
+  default     = "europe-west4"                              # we have to switch to europe-west4 because of redis
+}
+
+variable "GOOGLE_CLOUD_PROJECT_ID" {
+  type    = "string"
+  default = "shopsys-test-infra"
 }
